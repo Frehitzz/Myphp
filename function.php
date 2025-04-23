0a65843bbdb6eb43182ba3bc4147c67c4e62db21
Frehitzz <fritzharlydegamo@gmail.com>
2025-04-23 19:55:32 +0800

    Add files via upload

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+
+    function favecolor(){
+        echo "yello <br>";
+        echo "yello <br>";
+        echo "yello <br><br>";
+    }
+    //output the function 3 times you can do this
+    favecolor();
+    favecolor();
+    favecolor();
+    
+?>
